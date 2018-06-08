14feaf778ea7c2e83a1448cd8ace7fcc0b043d3b
Yorvi <jorvinrosario7@gmail.com>
2018-06-07 20:10:26 -0400

    setup scss file for future responsiveness

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name]) 
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :email, :password]) 
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password, :remember_me]) 
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :email, :password, :current_password, :remember_me]) 
   end
 end
